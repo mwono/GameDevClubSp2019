9d673fef3415d5a0a4635a03af70878c12c036e0
orellana412 <anthonyorellana21@gmail.com>
2019-04-18 12:33:36 -0700

    Added collectibles

---
+++ GameDevSp2019/Assets/Scripts/Collectible.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Collectible : MonoBehaviour
+{
+    public abstract void Collect();
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.name.Equals("player"))
+        {
+            updateScore();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GameDevSp2019/Assets/Scripts/Scythe.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scythe : Collectible
+{
+    public playerController pc;
+    
+    public override void Collect()
+    {
+        // pc.becomeInvincible();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
